1ea908e6b147e3b9acfe549d1485d87de02095db
root <root@6e50068a2e88>
2020-05-03 22:57:17 +0000

    Code_8 I don't keep track

---
+++ src/_01_variables/Variables.java
@@ -0,0 +1,10 @@
+package _01_variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+	
+	String nameOfSchool = "Jayden"; 
+	int numberOfStudents = 1000000;  
+	} 
+}
---
+++ src/_03_if_else/_1_unbirthday/Birthay.java
@@ -0,0 +1,8 @@
+package _03_if_else._1_unbirthday;
+
+public class Birthay {
+	public static void main(String[] args) {
+		String 
+	}
+
+}
