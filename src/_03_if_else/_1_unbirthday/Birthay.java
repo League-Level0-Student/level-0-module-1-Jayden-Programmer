8aff1f3464aba63302b76420cbe51d5dba2b7082
root <root@c9f56ff73769>
2020-05-10 22:56:56 +0000

    Code_9

--- src/_03_if_else/_1_unbirthday/Birthay.java
+++ src/_03_if_else/_1_unbirthday/Birthay.java
@@ -1,8 +1,16 @@
 package _03_if_else._1_unbirthday;
 
+import javax.swing.JOptionPane;
+
 public class Birthay {
-	public static void main(String[] args) {
-		String 
+	public static void main(String[] args) { 
+	String Birthay = JOptionPane.showInputDialog("enter your Birthay");
+	if(Birthay.equals("05/10/2020")){
+	JOptionPane.showMessageDialog(null, "Happy Birthay!");
+	
+	
+	}
+	else{JOptionPane.showInputDialog ("Happy Unbirthay!");}	
 	}
 
 }
